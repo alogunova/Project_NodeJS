56604bbb8b98af25333d70a1ff973542c9f2d589
alogunova <148359596+alogunova@users.noreply.github.com>
2023-10-21 22:55:41 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadFile('index.html'); // Здесь указывается имя вашего HTML файла
+
+  // Опционально: Открыть DevTools для отладки
+  // win.webContents.openDevTools();
+
+  // Опционально: Обработка события закрытия окна
+  win.on('closed', () => {
+    // Дополнительные действия при закрытии окна
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
